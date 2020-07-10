d99acec977017c988410ea77aeb7fce5c8a8fb83
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-07-09 21:43:27 -0700

    superagent partially done

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const pg = require('pg');
 const morgan = require('morgan');
 const  superagent= require('superagent');
+const { request } = require('express');
 
 //initialize express
 const app = express();
@@ -29,13 +30,46 @@ app.get('/',(req,res)=>{
   res.render('pages/searches/new');
 });
 
-app.post('/search-by-author',(req,res)=>{
-  console.log(req.body)
-});
-app.post('/search-by-title',(req,res)=>{
-  console.log(req.body)
 
-});
+//Need function to change http: to https: (card)
+app.post('/new-search', handleSearch);
+
+
+
+function handleSearch(req,res){
+const safeQuery = {
+  q : request.body.userInput,
+  search: request.body.value,
+  parameter: request.body.author || request.body.title,
+  // format: 'json'
+}
+const API = `https://www.googleapis.com/books/v1/volumes?q=+${safeQuery.parameter}:${safeQuery.q}`
+
+//&key=${safeQuery.key}
+
+superagent
+.get(API)
+// .query(safeQuery)
+
+  .then(data =>{
+    data.items.map(obj =>{
+      return new Book()
+
+    });
+  });
+
+
+}
+
+function Book (obj) {
+  this.title = obj.title || ('title is not available');
+  this.image = obj.image || ('https://i.imgur.com/J5LVHEL.jpg');
+  //authors is an array
+  this.author = obj.authors || ('author is unknown');
+  this.description = obj.description || ('description is not available');
+  this.isbn = obj.isbn || ('ISBN is not available');
+  this.categories = obj.categories || ('category is not available');
+}
 
 
 //start server
