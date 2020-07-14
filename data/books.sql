fe9646e2fe9db1f511b71c9ad24fd1740fd4b1e9
Ashley Casimir <ashcas@me.com>
2020-07-13 22:37:01 -0700

    finished seed.sql

--- data/books.sql
+++ data/books.sql
@@ -7,6 +7,6 @@ CREATE table books(
     title VARCHAR(255),
     isbn VARCHAR(255),
     image_url VARCHAR(255),
-    description VARCHAR(255),
+    description VARCHAR(500),
     bookshelf VARCHAR(255)
 );
